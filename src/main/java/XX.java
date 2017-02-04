a95889edb49c69c6b558a2a39cef28212f5fbdfc
Pawel <funky_pawel@o2.pl>
2017-02-04 21:52:22 +0100

    first commit

---
+++ src/main/java/XX.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Pawl on 2/4/2017.
+ */
+public class XX {
+
+    public static void main(String[]args){
+        System.out.println("CCCCCC");
+        System.out.println("CCCCCC");
+
+
+    }
+
+}
